49989cbb4d95ccf7fc5e80d8b89fa2be4c5c563d
Matthew Vernon <matthew@debian.org>
2011-08-17 22:52:41 +0100

    download scripts from buffyology.com

---
+++ buffyology.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#extract transcripts from buffology.com
+
+import urllib,re,os.path
+
+datadir=os.path.expanduser("~/tex/scripts/buffys5/html_buffyology")
+
+def geteppage(which):
+    url="http://www.buffyology.com/ep%d/" % which
+    text=urllib.urlopen(url).read()
+    match=re.search(r'<h3>Transcript:</h3>[^<]*<a href="([^"]*)">',
+                    text,re.MULTILINE)
+    if match != None:
+        return match.group(1)
+    else:
+        print "Unable to find episode!"
+
+def writeephtml(htmldir,which):
+    base=geteppage(which)
+    f=open("%s/%s" % (htmldir,base.split('/')[-1]),"w")
+    text=urllib.urlopen("http://www.buffyology.com/%s" % base).read()
+    print >>f, text
+    f.close()
+
+def getseries5():
+    for ep in range(79,101): #season 5 was episodes 79-100
+        writeephtml(datadir,ep)
+
+if __name__=="__main__":
+    getseries5()
