6dded73726728d4a1c84a15ba96e5f06aceba438
Matthew Vernon <matthew@debian.org>
2011-11-13 14:55:30 +0000

    Read casting in before doing chores-count
    This means we don't have the problem that if someone didn't have any
    chores, they wouldn't appear on the list. Also, we exclude "" from the
    output (otherwise that gave a count of chores no-one was down for yet,
    which wasn't exactly useful)

--- chorecount.py
+++ chorecount.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 
-f=open("chores.txt","r")
+import sys
+from parrot import basedir,statedir
 
 c={}
+
+f=open(statedir+"/casting.txt","r")
+for line in f:
+    line=line.strip()
+    if len(line)>0 and line[0]!="*":
+        l=line.split(':')
+        if len(l) > 1:
+            p=l[1].strip()
+            if p!='' and p !="Nobody" and p not in c:
+                c[p]=0
+f.close
+
+f=open(basedir+"/chores.txt","r")
 
 for line in f:
     if line.strip():
         b=line.split(':')
         for p in b[1].split(','):
             p=p.strip()
-            try:
-                c[p]+=1
-            except KeyError:
-                c[p]=1
+            if p!='':
+                try:
+                    c[p]+=1
+                except KeyError:
+                    c[p]=1
 
 for k,v in c.items():
     print k,v
