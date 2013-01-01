3ff283c7d29b4086acc8c5b8c663696eaf92e907
Matthew Vernon <matthew@debian.org>
2013-01-01 19:26:42 +0000

    deal with another non-ascii character

--- parrot.py
+++ parrot.py
@@ -648,6 +648,7 @@ def second_pass(fh,ft,epcast,pbe,allparts):
         l=l.replace(u'\xbe',"\\ae")
         l=l.replace(u'\xe0',"\\`{a}")
         l=l.replace(u'\xfc',"\\\"{u}")
+        l=l.replace(u'\xb8',"\\\"{u}") #Typo in the script
         l=l.replace("&amp;","\\&")
         l=re.sub(r'([^- ])- ',r'\1-',l) #foo- bar -> foo-bar
         if "<h2>" in l or "<h6>" in l:
